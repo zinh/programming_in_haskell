2b34602920c12b98b6aa7a36891d4248c44bed48
Vinh Nguyen <xuanvinhct@gmail.com>
2016-07-24 16:53:31 +0700

    update polynomial multiply

--- exercise/ex4/HW04.hs
+++ exercise/ex4/HW04.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 module HW04 where
+import Data.List (intercalate)
 
 newtype Poly a = P [a]
 
@@ -20,17 +21,32 @@ instance (Num a, Eq a) => Eq (Poly a) where
 -- Exercise 3 -----------------------------------------
 
 instance (Num a, Eq a, Show a) => Show (Poly a) where
-    show = undefined
+  show p = intercalate " + " (show' p 0 [])
+
+show' :: (Show a, Num a, Eq a) => Poly a -> Int -> [String] -> [String]
+show' (P []) _ results = results
+show' (P (m:ms)) current_power results 
+  | m == 0 = show' (P ms) (current_power + 1) results
+  | otherwise = show' (P ms) (current_power + 1) ((formatPower m current_power):results)
+  where formatPower item p
+          | item == 1 && p == 0 = show item
+          | item == 1 = "x^" ++ (show p)
+          | p == 0 = show item
+          | p == 1 = show item ++ "x"
+          | otherwise = (show item) ++ "x^" ++ (show p)
 
 -- Exercise 4 -----------------------------------------
 
 plus :: Num a => Poly a -> Poly a -> Poly a
-plus = undefined
+plus (P lst1) (P lst2) = P $ reverse (plusList lst1 lst2 [])
+  where
+    plusList [] lst results = (reverse lst) ++ results
+    plusList lst [] results = (reverse lst) ++ results
+    plusList (y:ys) (z:zs) results = plusList ys zs ((y + z):results)
 
 -- Exercise 5 -----------------------------------------
-
 times :: Num a => Poly a -> Poly a -> Poly a
-times = undefined
+times (P lst1) (P lst2) = foldl (\memo p -> memo + p)(P []) $foldl (\memo n -> (P (map (*n) lst1)) : memo) [] lst2
 
 -- Exercise 6 -----------------------------------------
 
